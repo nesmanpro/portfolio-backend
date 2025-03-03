92b98bdba7f88337b2513bc4f767d354bcb605f6
nesmanpro <nesmanpro@gmail.com>
2025-03-03 14:53:55 +0100

    update supabase port

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => {
       connection: {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
+        port: env.int('DATABASE_PORT', 10000),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
@@ -18,14 +18,6 @@ module.exports = ({ env }) => {
             true
           ),
         },
-        // ssl: env.bool('DATABASE_SSL', false) && {
-        // key: env('DATABASE_SSL_KEY', undefined),
-        // cert: env('DATABASE_SSL_CERT', undefined),
-        // ca: env('DATABASE_SSL_CA', undefined),
-        // capath: env('DATABASE_SSL_CAPATH', undefined),
-        // cipher: env('DATABASE_SSL_CIPHER', undefined),
-        //   rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        // },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
